047b0a3864b290ab83dd73538f7774c1c3646d65
Seojin Kim <seojkim@cx1r1s2.42seoul.kr>
2023-12-28 16:35:33 +0900

    realreal final

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,8 +6,21 @@
 /*   By: seojkim <seojkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 21:50:37 by seojkim           #+#    #+#             */
-/*   Updated: 2023/11/23 21:50:55 by seojkim          ###   ########.fr       */
+/*   Updated: 2023/12/27 15:38:31 by seojkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_printf(const char *, ...);
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdarg.h>
+# include "libft.h"
+
+int		ft_printf(const char *str, ...);
+int		print_args(char c, va_list *args, int *cnt);
+int		print_args2(char c, va_list *args, int *cnt);
+int		print_char(char c, int *cnt);
+int		print_str(char *s, int *cnt);
+int		print_nbr(long long n, char *dec, int *cnt);
+int		print_str_hex(unsigned int n, int hex_check, int *cnt);
+int		print_addr(void *h_addr, char *hex, int *cnt);
+#endif
